fd713db815d3f6d943c5b7940094c1e3d5384054
大溝桂 <kay.akubicharm.dev@gmail.com>
2024-11-21 23:27:15 +0900

    update

--- app/kafka-producer/src/main/java/com/redhat/MySpringBootRouter.java
+++ app/kafka-producer/src/main/java/com/redhat/MySpringBootRouter.java
@@ -20,9 +20,22 @@ public class MySpringBootRouter extends RouteBuilder {
                 .to("log:foo")
                 .end()
                 .to("stream:out")
+                .to("kafka:{{producer.topic}}?brokers={{kafka.bootstrap.url}}");
+    }
+
+    /*
+    @Override
+    public void configure() {
+        from("timer:hello?period={{timer.period}}").routeId("hello")
+                .transform().method("myBean", "saySomething")
+                .filter(simple("${body} contains 'foo'"))
+                .to("log:foo")
+                .end()
+                .to("stream:out")
                 .to("kafka:{{producer.topic}}?brokers={{kafka.bootstrap.url}}"
             + "&sslTruststoreLocation={{producer.ssl.truststore.location}}"
             + "&sslTruststorePassword={{producer.ssl.truststore.password}}"
             + "&securityProtocol={{producer.security.protocol}}");
-    }
+    }    
+    */
 }
